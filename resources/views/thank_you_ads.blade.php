4798e4a03f9e8f6170d463fc022903ba16d4a0d8
Michele Infante <infantemichele129@gmail.com>
2020-05-19 10:19:43 +0200

    indentato thanku e home

--- resources/views/thank_you_ads.blade.php
+++ resources/views/thank_you_ads.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.app')
 @section('content')
-<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
     
-  
     @if (session('ads.created'))
       <main role="main" class="inner cover">
         <h1 class="cover-heading">Grazie {{ Auth::user()->name }}</h1>
@@ -11,7 +10,6 @@
     @else
       <script>window.location = "/";</script>
     @endif
-  
     
   </div>
 @endsection
