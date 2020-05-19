6e9237e88eb00d6e30f85e5085ab5957d5081f87
Michele Infante <infantemichele129@gmail.com>
2020-05-19 10:24:06 +0200

    indentato add_ads + commenti

--- resources/views/add_ads.blade.php
+++ resources/views/add_ads.blade.php
@@ -4,25 +4,32 @@
 
 <div class="container">
   <div class="row">
+
     <div class="col-12">
+
+
+      {{-- Alert --}}
       <div>
         @if ($errors->any())
-        <div class="alert alert-danger">
-          <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-          </ul>
-        </div>
+          <div class="alert alert-danger">
+            <ul>
+              @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+              @endforeach
+            </ul>
+          </div>
         @endif
       </div>
+
+      {{-- Title --}}
       <form method="post" action="{{route('submit')}}" enctype="multipart/form-data">
         @csrf
         <div class="form-group">
           <label for="title">Titolo</label>
           <input type="text" class="form-control" name="title"  id="title" placeholder="inserisci il titolo dell'annuncio">
         </div>
-        
+
+        {{-- Category --}}
         <div class="form-group">
           <label for="category">Scegli la categoria</label>
           <select class="form-control" name="category_id" id="category">
@@ -33,18 +40,26 @@
             @endforeach
           </select>
         </div>
+
+        {{-- Price --}}
         <div class="form-group">
           <label for="price">Inserisci il prezzo</label>
           <input type="number" min="0"  class="form-control" name="price"  id="price" placeholder="Esempio 300">
         </div>
+
+        {{-- Description --}}
         <div class="form-group">
           <label for="description"> Descrivi il tuo prodotto</label>
           <textarea class="form-control" name="description" id="description" rows="3"></textarea>
         </div>
+
+        {{-- Image --}}
         <div class="form-group">
           <label for="img">Imagine</label>
           <input type="file" class="form-control" name="img"  id="image">
         </div>
+        
+        {{-- BUTTON --}}
         <div class="text-center">
           
           <button type="submit" class="btn btn-dark btn-lg w-100">
