6593f1491e350b9feac1af36a77b149f3f4a9482
Matteo Di Bartolomeo <mdb@tekny.eu>
2020-05-22 15:43:49 +0200

    Fixed Footer

--- resources/views/thank_you_ads.blade.php
+++ resources/views/thank_you_ads.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <div class="cover-container d-flex w-100 h-100 mt-5 pt-5">
     
     @if (session('ads.created'))
       <main role="main" class="inner cover">
