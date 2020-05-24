0d7e230c7911eedb0becc12d67564985d2bd6327
Matteo Di Bartolomeo <mdb@tekny.eu>
2020-05-24 18:25:21 +0200

    Aggiunta dashboared user

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -76,7 +76,9 @@
                                         <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
                                         <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                                         <path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
-                                      </svg>  
+                                      </svg> 
+                                      @elseif(Auth::user()->roles == 0) 
+
                                        @endif
                     
                                         {{ Auth::user()->name }}</span> <span class="caret"></span>
@@ -102,7 +104,13 @@
                                                      <span class="sr-only">unread notifies</span>
                                                 </button>
                                             </a>
-                                    @else
+                                    @elseif(Auth::user()->roles == 0)
+                                    <a href="{{ route('user.home') }}" target="_blank" rel="noopener noreferrer" class="dropdown-item">
+                                        <button type="button" class="btn background-main-color text-white">
+                                            Dashboard <span class="badge badge-light">3</span>
+                                             <span class="sr-only">unread notifies</span>
+                                        </button>
+                                    </a>
                                     @endif
                     
                                         <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
